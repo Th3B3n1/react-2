34910bcc1d6568871ef1cb10d7cd245ceceda83c
Th3B3n1 <othersc9bst@gmail.com>
2024-10-03 13:28:16 +0200

    TextFunctions done

--- src/TextFunctions.tsx
+++ src/TextFunctions.tsx
@@ -5,12 +5,16 @@ import { Header } from "./Header";
 export function TextFunctions()
 {
     const [text, setText] = useState("");
+    const [textStyle, setTextStyle] = useState({});
     return (
         <>
             <Header title={"Color Changer"} />
             <input type="text" onInput={(e) => setText(e.currentTarget.value)} />
-            <p>{text}</p>
+            <p style={textStyle}>{text}</p>
             <p>{text.length + " számú karaktert írt be a felhasználó" || "Még egy karaktert sem írt be a felhasználó."}</p>
+            <button onClick={() => setTextStyle({textTransform: "uppercase"})}>Nagybetűs</button>
+            <button onClick={() => setTextStyle({textTransform: "lowercase"})}>Kisbetűs</button>
+            <button onClick={() => setTextStyle({textTransform: "none"})}>Visszaállít</button>
             <Footer name={"Gipsz Jakab"} date={"2024-09-30"} time={"15"} aiUsed={[
                 "erre használtam",
                 "arra használtam",
